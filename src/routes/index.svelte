7e578771daaf57692ded641b63746b2484163055
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-04-09 16:51:22 -0700

    started

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,39 +1,90 @@
 <script lang="ts">
   import { io } from "socket.io-client";
-
-  import successkid from "images/successkid.jpg";
+  const dataTypes: Array<DataType> = [
+    {
+      value: "text",
+      text: "Text",
+    },
+    {
+      value: "number",
+      text: "Number",
+    },
+    {
+      value: "integer",
+      text: "Integer",
+    },
+    {
+      value: "character",
+      text: "Character",
+    },
+  ];
   import { onMount } from "svelte";
+  import Nav from "../components/Nav.svelte";
+  let parameters: Array<Parameter> = [
+    {
+      name: "Name",
+      type: dataTypes[0],
+      required: true,
+    },
+  ];
+
   onMount(() => {
     console.log("MOUNTED");
     const socket = io();
-	socket.emit('test')
-    socket.on('ok',(args) => {
-      console.log("ok",args);
+    socket.emit("test");
+    socket.on("ok", (args) => {
+      console.log("ok", args);
     });
-    // socket.on("connection", () => {
-    //   console.log("revieved connection from socket");
-
-    //   socket.emit('test')
-    // });
   });
-</script>
 
+  interface Parameter {
+    name: string;
+    type: DataType;
+    required: boolean;
+  }
+
+  interface DataType {
+    value: string;
+    text: string;
+  }
+</script>
+<style>
+	th {
+		text-align: start;
+		padding: 8px;
+	}
+</style>
 <svelte:head>
   <title>Unified Data Modelers</title>
 </svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-  <img alt="Success Kid" src={successkid} />
-  <figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p>
-  <strong
-    >Try editing this file (src/routes/index.svelte) to test live reloading.</strong
-  >
-</p>
-
-<style>
-</style>
+<Nav />
+<table>
+	<tr>
+		<th>
+			Name
+		</th>
+		<th>
+			Required
+		</th>
+		<th>
+			Type
+		</th>
+	</tr>
+  {#each parameters as paramater}
+    <tr>
+      <td><input value={paramater.name} /></td>
+      <td><input checked={paramater.required} type="checkbox" /></td>
+      <td
+        ><select name="DataTypes">
+          {#each dataTypes as type}
+            <option value={type.value}>{type.text}</option>
+          {/each}
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+        </select></td
+      >
+    </tr>
+  {/each}
+</table>

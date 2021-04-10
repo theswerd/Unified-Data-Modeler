13e73177af7d4bbb426a36f9ef3ef58a68c6b5ba
Ethan Hopkins <58455740+GlowingEmber@users.noreply.github.com>
2021-04-10 13:32:27 -0700

    stylezzz

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -100,7 +100,7 @@
     {#each parameters as paramater, index}
       <tr>
         <td><input bind:value={paramater.name} /></td>
-        <td><input bind:checked={paramater.required} type="checkbox" /></td>
+        <td><label class="container"><input bind:checked={paramater.required} type="checkbox" /><span class="checkmark"></span></label></td>
         <td
           ><select
             name="DataTypes"
@@ -122,9 +122,103 @@
 <input  type="file" bind:files accept=".yaml">
 
 <style>
+  /* Customize the label (the container) */
+.container {
+  display: block;
+  position: relative; 
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.container input {
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-color: #BF0A30;
+  transition-duration: 500ms;
+}
+
+/* On mouse-over, add a grey background color */
+/* .container:hover input ~ .checkmark {
+  background-color: #ccc;
+} */
+
+/* When the checkbox is checked, add a blue background */
+.container input:checked ~ .checkmark {
+  background-color: #00b518;
+  transition-duration: 300ms;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.container input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.container .checkmark:after {
+  left: 9px;
+  top: 5px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+  transition-duration: 500ms;
+}
+  table {
+    margin: 0 auto;
+    border-collapse: collapse;
+    border: 2px solid whitesmoke;
+	background-color: white;
+	opacity: 1;
+  }
+
+  input:active ,
+  input:focus  {
+    border: none;
+    outline: none;
+  }
+
+  table td ,
+  table th  {
+    border: 2px solid whitesmoke;
+  }
+  input {
+    margin: auto;
+    text-align: center;
+    padding: 5px;
+    border: none;
+    align-self: center;
+    box-sizing: border-box;
+  }
+
   th {
-    text-align: start;
+    text-align: center;
     padding: 8px;
+
   }
 
   input{
@@ -133,8 +227,27 @@
   td {
     text-align: center;
   }
+   
   :global(body) {
     font-family: 'Courier Prime', monospace;
+    
   }
-  
+  button {
+    font-family: 'Courier Prime', monospace;
+  }
+  input {
+    font-family: 'Courier Prime', monospace;
+  } 
+
+@keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+}
 </style>

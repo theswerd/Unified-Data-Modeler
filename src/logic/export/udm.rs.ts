81392482315881f151b251e1a2adb547c7fa8484
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-04-10 20:37:11 -0700

    rust is operational

--- src/logic/export/udm.rs.ts
+++ src/logic/export/udm.rs.ts
@@ -1,9 +1,19 @@
-
 import type { Parameter } from "../../models/parameter";
 import { syntaxTree, Languages, flatSyntax, DataType } from "../syntax_tree";
 import type from "./type_finder";
+import warning from "./warning";
 
 export default (name: string, parameters: Array<Parameter>): string => {
-    
-    return ""
-}
+  return (
+    warning +
+    "\nstruct " +
+    name +
+    " {\n" +
+    parameters
+      .map((value) => {
+        return "    " + name + ": " + type(value.type, Languages.Rust);
+      })
+      .join(",\n") +
+    "\n}"
+  );
+};

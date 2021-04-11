2f7cb856a1f0989e9017fa115271be4a631a3124
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-04-10 20:49:36 -0700

    suring up

--- src/logic/export/udm.rs.ts
+++ src/logic/export/udm.rs.ts
@@ -1,26 +1,33 @@
 import type { Parameter } from "../../models/parameter";
-import { syntaxTree, Languages, flatSyntax, DataType } from "../syntax_tree";
+import { Languages } from "../syntax_tree";
 import type from "./type_finder";
 import warning from "./warning";
 
 export default (name: string, parameters: Array<Parameter>): string => {
   return (
     warning +
-    +"\n"
-    +`
+    +"\n" +
+    `
     // Add these lines to the [dependencies] section of your cargo.toml:
     // serde = { version = "1.0", features = ["derive"] }
     // serde_json = "1.0"
 
     use serde::{Serialize, Deserialize};
     #[derive(Serialize, Deserialize, Debug)]
-    `+
+    ` +
     "\nstruct " +
     name +
     " {\n" +
     parameters
       .map((value) => {
-        return "    " + name + ": " +(value.required?"":"Option<")+ type(value.type, Languages.Rust)+(value.required?"":">");
+        return (
+          "    " +
+          name +
+          ": " +
+          (value.required ? "" : "Option<") +
+          type(value.type, Languages.Rust) +
+          (value.required ? "" : ">")
+        );
       })
       .join(",\n") +
     "\n}"

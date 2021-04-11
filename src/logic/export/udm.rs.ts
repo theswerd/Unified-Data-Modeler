a2dadd9967a474da860e4f4dfc66179ad7bba1d3
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-04-10 20:48:08 -0700

    upgraded rust

--- src/logic/export/udm.rs.ts
+++ src/logic/export/udm.rs.ts
@@ -6,12 +6,21 @@ import warning from "./warning";
 export default (name: string, parameters: Array<Parameter>): string => {
   return (
     warning +
+    +"\n"
+    +`
+    // Add these lines to the [dependencies] section of your cargo.toml:
+    // serde = { version = "1.0", features = ["derive"] }
+    // serde_json = "1.0"
+
+    use serde::{Serialize, Deserialize};
+    #[derive(Serialize, Deserialize, Debug)]
+    `+
     "\nstruct " +
     name +
     " {\n" +
     parameters
       .map((value) => {
-        return "    " + name + ": " + type(value.type, Languages.Rust);
+        return "    " + name + ": " +(value.required?"":"Option<")+ type(value.type, Languages.Rust)+(value.required?"":">");
       })
       .join(",\n") +
     "\n}"
